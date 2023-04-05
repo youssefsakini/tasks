e7f0910a2124dd0a4dd9323ea41353c34245fada
youssefsakini <youssef0sakini@gmail.com>
2023-04-05 13:38:51 +0000

    Fill tasks details component and fix code issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TasksViewComponent } from './pages/tasks-view/tasks-view.component';
-import { TasksSubmenuComponent } from './pages/tasks-submenu/tasks-submenu.component';
-import { TasksDetailsComponent } from './pages/tasks-details/tasks-details.component';
+import { TasksViewComponent } from './components/tasks-view/tasks-view.component';
+import { TasksSubmenuComponent } from './components/tasks-submenu/tasks-submenu.component';
+import { TasksDetailsComponent } from './components/tasks-details/tasks-details.component';
+import { LayoutsComponent } from './layouts/layouts.component';
 @NgModule({
   declarations: [
     AppComponent,
     TasksViewComponent,
     TasksSubmenuComponent,
-    TasksDetailsComponent
+    TasksDetailsComponent,
+    LayoutsComponent
   ],
   imports: [
     BrowserModule
